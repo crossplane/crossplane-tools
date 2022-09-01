7500664fa5ad567dc7287ac57fb2d171314d9a39
Ruhika <ruhika406@gmail.com>
2022-09-01 14:03:50 +0530

    testBreakingChanges: checking breaking changes between 2 crds and also writing unit test for the same
    Signed-off-by: Ruhika <ruhika406@gmail.com>

---
+++ cmd/breakingChanges/main_test.go
@@ -0,0 +1,292 @@
+/*
+Copyright 2022 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+func TestBreakingChanges(t *testing.T) {
+
+	type args struct {
+		oldyaml *v1.JSONSchemaProps
+		newyaml *v1.JSONSchemaProps
+	}
+	type want struct {
+		result []string
+	}
+	cases := map[string]struct {
+		args
+		want
+	}{
+		"No changes": {
+			args: args{
+				oldyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"apiVersion": {},
+						"kind":       {},
+						"metadata":   {},
+						"status": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"conditions": {},
+								"atProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"id": {},
+									},
+								},
+							},
+						},
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+								"forProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"description":             {},
+										"enableInboundForwarding": {},
+										"enableLogging":           {},
+										"networks":                {},
+										"alternativeNameServerConfig": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"targetNameServers": {},
+											},
+										},
+									},
+								},
+								"providerRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+									},
+								},
+								"providerConfigRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+									},
+								},
+								"publishConnectionDetailsTo": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+										"configRef": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"name": {},
+											},
+										},
+										"metadata": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"type":        {},
+												"annotations": {},
+												"labels":      {},
+											},
+										},
+									},
+								},
+								"writeConnectionSecretToRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name":      {},
+										"namespace": {},
+									},
+								},
+							},
+						},
+					},
+				},
+
+				newyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"apiVersion": {},
+						"kind":       {},
+						"metadata":   {},
+						"status": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"conditions": {},
+								"atProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"id": {},
+									},
+								},
+							},
+						},
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+								"forProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"description":             {},
+										"enableInboundForwarding": {},
+										"enableLogging":           {},
+										"networks":                {},
+										"alternativeNameServerConfig": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"targetNameServers": {},
+											},
+										},
+									},
+								},
+								"providerRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+									},
+								},
+								"providerConfigRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+									},
+								},
+								"publishConnectionDetailsTo": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name": {},
+										"configRef": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"name": {},
+											},
+										},
+										"metadata": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"type":        {},
+												"annotations": {},
+												"labels":      {},
+											},
+										},
+									},
+								},
+								"writeConnectionSecretToRef": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"name":      {},
+										"namespace": {},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+
+		"spec.forProvider.description": {
+			args: args{
+				oldyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+								"forProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"description":             {},
+										"enableInboundForwarding": {},
+										"enableLogging":           {},
+										"networks":                {},
+										"alternativeNameServerConfig": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"targetNameServers": {},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				newyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+								"forProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"enableInboundForwarding": {},
+										"enableLogging":           {},
+										"networks":                {},
+										"alternativeNameServerConfig": {
+											Properties: map[string]v1.JSONSchemaProps{
+												"targetNameServers": {},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				result: []string{"spec.forProvider.description"},
+			},
+		},
+
+		"kind": {
+			args: args{
+				oldyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"apiVersion": {},
+						"kind":       {},
+						"metadata":   {},
+						"status": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"conditions": {},
+								"atProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"id": {},
+									},
+								},
+							},
+						},
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+							},
+						},
+					},
+				},
+				newyaml: &v1.JSONSchemaProps{
+					Properties: map[string]v1.JSONSchemaProps{
+						"apiVersion": {},
+						"metadata":   {},
+						"status": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"conditions": {},
+								"atProvider": {
+									Properties: map[string]v1.JSONSchemaProps{
+										"id": {},
+									},
+								},
+							},
+						},
+						"spec": {
+							Properties: map[string]v1.JSONSchemaProps{
+								"deletionPolicy": {},
+							},
+						},
+					},
+				},
+			},
+			want: want{
+				result: []string{"kind"},
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := PrintFields(tc.args.oldyaml, "", tc.args.newyaml)
+			if diff := cmp.Diff(got, tc.want.result); diff != "" {
+				t.Errorf("PrintFields(...): -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -6,15 +6,32 @@ require (
 	github.com/dave/jennifer v1.4.1
 	github.com/google/go-cmp v0.5.6
 	github.com/pkg/errors v0.9.1
-	golang.org/x/tools v0.1.5
+	golang.org/x/tools v0.1.12
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210912230133-d1bdfacee922 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/apiextensions-apiserver v0.25.0 // indirect
+	k8s.io/apimachinery v0.25.0 // indirect
+	k8s.io/klog/v2 v2.70.1 // indirect
+	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
+	sigs.k8s.io/json v0.0.0-20220713155537-f223a00ba0e2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 )
